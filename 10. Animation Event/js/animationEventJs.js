7f31a8c2c24b43d0eb39b3211f8cdf9548a356fd
Amen0920 <luj0605@gmail.com>
2021-05-12 17:24:38 +0900

    commit

---
+++ 10. Animation Event/js/animationEventJs.js
@@ -0,0 +1,15 @@
+(function(){
+    const ballElem = document.querySelector('.ball');
+
+    ballElem.addEventListener('click',function(){
+        ballElem.style.animation = 'ball-ani  alternate 1s 3 forwards';
+    });
+
+    ballElem.addEventListener('animationend',function(e){
+        ballElem.classList.add('end');
+    });
+    ballElem.addEventListener('animationiteration',function(){
+        console.log('반복!');
+    })
+
+})();
