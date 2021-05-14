2aaf4406d42e7b3129c073ccca2f21524fa6a157
Amen0920 <luj0605@gmail.com>
2021-05-14 22:13:41 +0900

    scroll

---
+++ 11. Final_Example/js/wall3d.js
@@ -0,0 +1,13 @@
+(function(){
+    const houseElem = document.querySelector('.house');
+
+    let maxScrollValue = document.body.offsetHeight - window.innerHeight
+
+    window.addEventListener('scroll',function(){
+        const zMove = pageYOffset / maxScrollValue * 980;
+        
+        houseElem.style.transform = `translateZ(${zMove-490}vw)`
+        
+    });
+
+})();
