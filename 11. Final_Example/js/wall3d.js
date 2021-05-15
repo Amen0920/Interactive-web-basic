f62eed16f836169bab2ba0c6133cc8bf65b55e36
Amen0920 <luj0605@gmail.com>
2021-05-15 13:17:01 +0900

    commit

--- 11. Final_Example/js/wall3d.js
+++ 11. Final_Example/js/wall3d.js
@@ -1,21 +1,41 @@
 (function(){
+    //DOM Elem
     const houseElem = document.querySelector('.house');
+    const barElem = document.querySelector('.progress-bar')
+    const stageElem = document.querySelector('.stage');
+
+    const mousePos = { x: 0, y: 0};
 
     let maxScrollValue;
 
-    window.addEventListener('scroll',function(){
-        const zMove = pageYOffset / maxScrollValue * 980;
-        
-        houseElem.style.transform = `translateZ(${zMove-490}vw)`
-        
-    });
 
+    //resize function 
     function resizeHandler(){
         maxScrollValue = document.body.offsetHeight - window.innerHeight;
     }
 
+    //scroll function
+    function scollHandler(){
+         // 3D 
+         const scollPer = pageYOffset / maxScrollValue;
+         const zMove =  scollPer * 980 - 490;
+         houseElem.style.transform = `translateZ(${zMove}vw)`
+ 
+         //progressbar 
+         barElem.style.width = `${scollPer*100}%`
+    }
 
-    window.addEventListener('resize',resizeHandler)
+    //mouse MOVE function 
+    function mouseMoveHandler(e){
+        mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+        mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+        
+        stageElem.style.transform = `rotateX(${mousePos.y*5}deg) rotateY(${mousePos.x*5}deg)`
+    }
+
+    window.addEventListener('scroll',scollHandler);
+    window.addEventListener('resize',resizeHandler);
+    window.addEventListener('mousemove', mouseMoveHandler);
     
     resizeHandler();
 
