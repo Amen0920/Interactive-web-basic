486b9d2cc0923e0c17fcac6ba5978bf706beebc1
Amen0920 <luj0605@gmail.com>
2021-05-17 15:44:26 +0900

    commit

--- 11. Final_Example/js/Character.js
+++ 11. Final_Example/js/Character.js
@@ -34,8 +34,14 @@ function Character(info){
     document.querySelector('.stage').appendChild(this.mainElem);
    
     this.mainElem.style.left = `${info.xPos}%`;
-
+    // 스크롤중인지 아닌자 확인하는 속성값
     this.scrollState = false;
+
+    // 바로 이전 스크롤 위치
+    this.lastScrollTop = 0;
+    
+    this.xPos = info.xPos;
+    this.speed = 1;
     this.init();
     
 }
@@ -47,7 +53,6 @@ Character.prototype = {
 
         window.addEventListener('scroll',function(){
             clearTimeout(self.scrollState);
-            console.log('clear Self : '+self.scrollState)
 
             if(!self.scrollState){
                 self.mainElem.classList.add('running');
@@ -58,6 +63,34 @@ Character.prototype = {
                 self.mainElem.classList.remove('running');
             },500);
             
+            if(self.lastScrollTop > pageYOffset){
+                // 이전 스크롤 위치가 크다면 : 스크를 올림
+                self.mainElem.setAttribute('data-direction','backward');
+            }else {
+                //현재 스크롤 위치가 크다면 스크롤 내림
+                self.mainElem.setAttribute('data-direction','forward');
+            }
+
+            self.lastScrollTop = pageYOffset;
+
         });
+
+        window.addEventListener('keydown',function(e){
+            console.log(e.key)
+            if(e.key === 'ArrowLeft'){
+                self.mainElem.setAttribute('data-direction', 'left');
+                self.mainElem.classList.add('running');
+                self.xPos -= self.speed;
+                self.mainElem.style.left = `${self.xPos}%`
+                
+            }else if(e.key === 'ArrowRight'){
+                self.mainElem.setAttribute('data-direction', 'right');
+                self.mainElem.classList.add('running');
+            }
+        });
+        
+        window.addEventListener('keyup',function(){
+            self.mainElem.classList.remove('running');
+        })
     }
 };
